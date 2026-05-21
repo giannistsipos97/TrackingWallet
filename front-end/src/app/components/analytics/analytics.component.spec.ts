a238af32db274ad38beb0a89a7378d538bf6b213
Giannis Tsipos <gtsipos@outlook.com>
2026-05-21 12:54:15 +0300

    creating analytics component for later use + analytics service

---
+++ front-end/src/app/components/analytics/analytics.component.spec.ts
@@ -0,0 +1,27 @@
+import { ComponentFixture, TestBed } from '@angular/core/testing';
+import { provideHttpClient } from '@angular/common/http';
+import { provideHttpClientTesting } from '@angular/common/http/testing';
+import { AnalyticsComponent } from './analytics.component';
+
+describe('AnalyticsComponent', () => {
+  let component: AnalyticsComponent;
+  let fixture: ComponentFixture<AnalyticsComponent>;
+
+  beforeEach(async () => {
+    await TestBed.configureTestingModule({
+      imports: [AnalyticsComponent],
+      providers: [
+        provideHttpClient(), // Provides HTTP client infrastructure
+        provideHttpClientTesting(), // Mock backend context runner
+      ],
+    }).compileComponents();
+
+    fixture = TestBed.createComponent(AnalyticsComponent);
+    component = fixture.componentInstance;
+    fixture.detectChanges();
+  });
+
+  it('should create', () => {
+    expect(component).toBeTruthy();
+  });
+});
---
+++ front-end/src/app/services/analytics.service.spec.ts
@@ -0,0 +1,33 @@
+import { TestBed } from '@angular/core/testing';
+import { provideHttpClient } from '@angular/common/http';
+import {
+  provideHttpClientTesting,
+  HttpTestingController,
+} from '@angular/common/http/testing';
+import { AnalyticsService } from './analytics.service';
+
+describe('AnalyticsService', () => {
+  let service: AnalyticsService;
+  let httpMock: HttpTestingController;
+
+  beforeEach(() => {
+    TestBed.configureTestingModule({
+      providers: [
+        AnalyticsService,
+        provideHttpClient(), // Core HTTP infrastructure
+        provideHttpClientTesting(), // Mock backend context utility
+      ],
+    });
+
+    service = TestBed.inject(AnalyticsService);
+    httpMock = TestBed.inject(HttpTestingController);
+  });
+
+  afterEach(() => {
+    httpMock.verify();
+  });
+
+  it('should create the analytics service instance cleanly', () => {
+    expect(service).toBeTruthy();
+  });
+});
