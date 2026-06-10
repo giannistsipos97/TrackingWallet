93d33f4f00fbf77ab601816acb721eb3f64add36
Giannis Tsipos <gtsipos@outlook.com>
2026-06-10 12:48:46 +0300

    change to ngClass where needed

--- front-end/src/app/components/auth/register/register.component.ts
+++ front-end/src/app/components/auth/register/register.component.ts
@@ -2,11 +2,12 @@ import { Component, computed, inject, signal } from '@angular/core';
 import { FormsModule } from '@angular/forms';
 import { AuthService } from '../../../services/auth.service';
 import { Router } from '@angular/router';
+import { CommonModule } from '@angular/common';
 
 @Component({
   selector: 'app-register',
   standalone: true,
-  imports: [FormsModule],
+  imports: [FormsModule, CommonModule],
   templateUrl: './register.component.html',
   styleUrl: './register.component.scss',
 })
--- front-end/src/app/components/drawer/drawer.component.ts
+++ front-end/src/app/components/drawer/drawer.component.ts
@@ -1,11 +1,12 @@
 import { Component, inject } from '@angular/core';
 import { HeaderService } from '../../services/header.service';
 import { RouterLink } from '@angular/router';
+import { CommonModule } from '@angular/common';
 
 @Component({
   selector: 'app-drawer',
   standalone: true,
-  imports: [RouterLink],
+  imports: [RouterLink, CommonModule],
   templateUrl: './drawer.component.html',
   styleUrl: './drawer.component.scss',
 })
