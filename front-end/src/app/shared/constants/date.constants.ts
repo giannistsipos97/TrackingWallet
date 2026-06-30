c9719806c7b7b811f8be285b45b52692e4fd94ea
Giannis Tsipos <gtsipos@outlook.com>
2026-06-30 13:07:26 +0300

    create a global costraint for months

---
+++ front-end/src/app/shared/constants/date.constants.ts
@@ -0,0 +1,14 @@
+export const GLOBAL_MONTHS: string[] = [
+  'January',
+  'February',
+  'March',
+  'April',
+  'May',
+  'June',
+  'July',
+  'August',
+  'September',
+  'October',
+  'November',
+  'December',
+];
